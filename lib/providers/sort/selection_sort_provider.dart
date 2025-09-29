a3ceb69d8a583c3f4515a3bc304bc20723149660
Himanshu Singhal <himanshu17113@gmail.com>
2025-09-29 18:50:01 +0530

    Updated selection sort

--- lib/providers/sort/selection_sort_provider.dart
+++ lib/providers/sort/selection_sort_provider.dart
@@ -2,7 +2,7 @@ import 'package:algo_vis/providers/sort/sort_provider.dart';
 
 class SelectionSortProvider extends SortProvider {
   @override
-  Future<void> onExecute() async {
+  Future<void> onExecute([int? value]) async {
     await selectionSort();
   }
 
